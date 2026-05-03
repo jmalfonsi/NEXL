d63fc0dd0e09d820b66c757e46b38ed4170c3701
JMA <jmainformatique@gmail.com>
2026-05-03 19:29:27 +0200

    Delete .env directory

--- .env/lib/python3.12/site-packages/outlines/processors/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Processors to control generation in steerable models."""
-
-from .base_logits_processor import OutlinesLogitsProcessor
-
-__all__ = [
-    "OutlinesLogitsProcessor",
-]
--- .env/lib/python3.12/site-packages/outlines/types/locale/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Locale-specific regex patterns."""
-
-from . import us
-
-__all__ = [
-    "us",
-]
